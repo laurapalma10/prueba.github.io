f4afc53a6c81af05ef0d1833bf1c0f55b7190724
laurapalma10 <129076902+laurapalma10@users.noreply.github.com>
2023-04-24 09:27:05 -0500

    main

---
+++ js/operador.html.js
@@ -0,0 +1,11 @@
+//Conversion larga:
+var celsius1 = promt("Introduzca la temperatura en grados celcius"); 
+celsius1 = parseFloat(celsius1); 
+
+//Conversion corta:
+//var celsius1 = parseFloat(promt("Introduzca la temperatura en grados celsius:")); 
+var Farenheit1 = 9/5*celsius1+32; 
+
+document.write("De Celsius a Farenheit es: ", Farenheit1);
+
+
